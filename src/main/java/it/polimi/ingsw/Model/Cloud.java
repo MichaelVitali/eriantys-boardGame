09120b71c2a55653407b6e9822b1720bd42a80cb
MichaelVitali <michael.vitali@mail.polimi.it>
2022-03-22 18:00:31 +0100

    tile elimination

--- src/main/java/it/polimi/ingsw/Model/Cloud.java
+++ src/main/java/it/polimi/ingsw/Model/Cloud.java
@@ -1,9 +1,26 @@
 package it.polimi.ingsw.Model;
 
-public class Cloud extends Tile {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cloud {
     private int numberOfStudents;
+    private List<Student> students;
 
     public Cloud(int numberOfStudents) {
+        this.students = new ArrayList<>();
         this.numberOfStudents = numberOfStudents;
+    }
+
+    public void addStudents(List<Student> newStudent) {
+        students.addAll(newStudent);
+    }
+
+    public boolean removeStudent(Student studentToBeRemoved) { // Oppure passo il colore?
+        return students.remove(studentToBeRemoved);
+    }
+
+    public List<Student> getStudents() {
+        return students;
     }
 }
--- src/main/java/it/polimi/ingsw/Model/Island.java
+++ src/main/java/it/polimi/ingsw/Model/Island.java
@@ -1,11 +1,14 @@
 package it.polimi.ingsw.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Island extends Tile {
+public class Island {
     private Tower tower;
+    private List<Student> students;
 
     public Island() {
+        students = new ArrayList<>();
     }
 
     public void setTower(List<Tower> t){
@@ -18,5 +21,13 @@ public class Island extends Tile {
 
     public int getAggregation(){
         return 1;
+    }
+
+    public void addStudents(Student newStudent) {
+        students.add(newStudent);
+    }
+
+    public List<Student> getStudents() {
+        return students;
     }
 }
