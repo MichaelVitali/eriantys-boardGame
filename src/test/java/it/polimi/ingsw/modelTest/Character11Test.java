e0bc21528d3e3d868055383c480e6b6724072cdf
Manuel Tacca <manueltacca@outlook.com>
2022-05-01 11:41:38 +0200

    Character11 class complete with tests

---
+++ src/test/java/it/polimi/ingsw/modelTest/Character11Test.java
@@ -0,0 +1,53 @@
+package it.polimi.ingsw.modelTest;
+
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.model.exception.EmptyBagException;
+import it.polimi.ingsw.model.exception.EmptyTableException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class Character11Test {
+
+    private Character11 character = new Character11(11, 0, 4);
+    private Round round;
+
+    @Before
+    public void setUp() throws Exception {
+        List<String> nicknames = new ArrayList<>();
+        nicknames.add("mike");
+        nicknames.add("enri");
+        round = new Round(new ExpertGame(2, nicknames));
+    }
+
+    @Test
+    public void testDoYourJob() throws EmptyBagException, EmptyTableException {
+
+        Student chosen;
+
+        character.addStudents(round.getGame().getGameTable().getBag().drawStudents(4));
+        character.activateEffect(0, round);
+        chosen = character.getStudentsOnCard()[2];
+        character.doYourJob(0, 2);
+        assertEquals(chosen, round.getGame().getGameTable().getSchoolBoards()[0].removeStudentFromTable(chosen.getColor()));
+        assertNotEquals(chosen, character.getStudentsOnCard()[2]);
+    }
+
+    @Test
+    public void testActivateEffect() {
+        assertEquals(character.activateEffect(0, round), character);
+        assertEquals(0, character.getRound().getRoundState());
+    }
+
+    @Test
+    public void testSetRoundState() {
+        character.setRoundState(1);
+        assertEquals(1, character.getRoundState());
+        character.setRoundState(7);
+        assertEquals(-1, character.getRoundState());
+    }
+}
