b9ee52cb99be8553b2c0895858b4abec47bd6d83
Manuel Tacca <manueltacca@outlook.com>
2022-05-02 18:57:51 +0200

    Character12 class complete with tests

---
+++ src/main/java/it/polimi/ingsw/model/Character12.java
@@ -0,0 +1,71 @@
+package it.polimi.ingsw.model;
+
+import it.polimi.ingsw.model.exception.EmptyTableException;
+import it.polimi.ingsw.model.exception.InvalidIndexException;
+
+import java.util.Arrays;
+
+public class Character12 extends Character{
+
+    private PawnColor pawnColor;
+
+    public Character12(int id, int cost){
+        super(id, cost);
+        pawnColor = null;
+    }
+
+    @Override
+    public void doYourJob(int playerId, int parameter) {
+        if(getRoundState() == 5){
+            try{
+                if(parameter < 0 || parameter > 4)
+                    throw new InvalidIndexException("Error effect 12: invalid pawncolor");
+
+                switch (parameter){
+                    case 0:
+                        pawnColor = PawnColor.YELLOW;
+                        break;
+                    case 1:
+                        pawnColor = PawnColor.BLUE;
+                        break;
+                    case 2:
+                        pawnColor = PawnColor.GREEN;
+                        break;
+                    case 3:
+                        pawnColor = PawnColor.RED;
+                        break;
+                    case 4:
+                        pawnColor = PawnColor.PINK;
+                        break;
+                }
+
+                for(int i=0; i<getRound().getGame().getNumberOfPlayers(); i++)
+                    for (int j=0; j<3; j++)
+                        if(getRound().getGame().getGameTable().getSchoolBoards()[i].getNumberOfStudentsOnTable(pawnColor) > 0)
+                            getRound().getGame().getGameTable().getBag().addStudents(Arrays.asList(getRound().getGame().getGameTable().getSchoolBoards()[i].removeStudentFromTable(pawnColor)));
+
+
+            }catch (InvalidIndexException e){
+                ////////////////////////////////
+            }catch ( EmptyTableException e){
+                ////////////////////////////////
+            }
+
+            deactivateEffect();
+        }
+    }
+
+    @Override
+    public Round activateEffect (int playerID, Round round) {
+        round.getGame().getPlayer(playerID).setErrorMessage("Select pawn color");
+        setRoundState(5);
+        return super.activateEffect(playerID, round);
+    }
+
+    @Override
+    public void setRoundState(int state){
+        if (state>=0 && state<6)
+            this.roundState=state;
+        else roundState = -1;
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/modelTest/Character12Test.java
@@ -0,0 +1,55 @@
+package it.polimi.ingsw.modelTest;
+
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.model.exception.EmptyTableException;
+import it.polimi.ingsw.model.exception.FullTableException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class Character12Test {
+
+    private Character12 character;
+    private Round round;
+
+    @Before
+    public void setUp() throws Exception {
+        character = new Character12(10, 0);
+        List<String> nicknames = new ArrayList<>();
+        nicknames.add("mike");
+        nicknames.add("enri");
+        round = new Round(new ExpertGame(2, nicknames));
+    }
+
+    @Test
+    public void testDoYourJob() throws FullTableException {
+        character.activateEffect(0, round);
+        round.getGame().getGameTable().getSchoolBoards()[0].addStudentOnTable(new Student(PawnColor.YELLOW));
+        round.getGame().getGameTable().getSchoolBoards()[0].addStudentOnTable(new Student(PawnColor.YELLOW));
+        round.getGame().getGameTable().getSchoolBoards()[0].addStudentOnTable(new Student(PawnColor.YELLOW));
+        round.getGame().getGameTable().getSchoolBoards()[0].addStudentOnTable(new Student(PawnColor.YELLOW));
+        round.getGame().getGameTable().getSchoolBoards()[1].addStudentOnTable(new Student(PawnColor.YELLOW));
+        round.getGame().getGameTable().getSchoolBoards()[1].addStudentOnTable(new Student(PawnColor.YELLOW));
+        character.doYourJob(0, 0);
+        assertEquals(1, round.getGame().getGameTable().getSchoolBoards()[0].getNumberOfStudentsOnTable(PawnColor.YELLOW));
+        assertEquals(0, round.getGame().getGameTable().getSchoolBoards()[1].getNumberOfStudentsOnTable(PawnColor.YELLOW));
+    }
+
+    @Test
+    public void testActivateEffect() {
+        assertEquals(character.activateEffect(0, round), character);
+        assertEquals(0, character.getRound().getRoundState());
+    }
+
+    @Test
+    public void testSetRoundState() {
+        character.setRoundState(1);
+        assertEquals(1, character.getRoundState());
+        character.setRoundState(6);
+        assertEquals(-1, character.getRoundState());
+    }
+}
