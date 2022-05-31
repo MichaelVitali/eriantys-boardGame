d70d871015cd3e5c7df5425b67f23510aeb919d0
MichaelVitali <michael.vitali@mail.polimi.it>
2022-05-31 12:47:00 +0200

    waiting scene

--- src/main/java/it/polimi/ingsw/client/gui/guiControllers/MatchPlayersController.java
+++ src/main/java/it/polimi/ingsw/client/gui/guiControllers/MatchPlayersController.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.client.gui.guiControllers;
 
 import it.polimi.ingsw.controller.message.ConnectionState;
+import it.polimi.ingsw.controller.message.GameMessage;
 import it.polimi.ingsw.controller.message.Message;
 import it.polimi.ingsw.controller.message.SetupMessage;
 import javafx.application.Platform;
@@ -14,6 +15,7 @@ import javafx.scene.shape.Circle;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class MatchPlayersController extends GuiController{
 
@@ -36,39 +38,60 @@ public class MatchPlayersController extends GuiController{
             if (setupMessage.getConnectionState() == ConnectionState.NUMBEROFPLAYERS) {
                 Platform.runLater(new Runnable() {
                                       @Override
-                                      public void run() {
-                                          matchPlayersMessage.setText(setupMessage.getMessage());
+                                      public void run() {matchPlayersMessage.setText(setupMessage.getMessage());
                                       }
                                   }
                 );
             } else if (setupMessage.getConnectionState() == ConnectionState.SUCCESS) {
-                Platform.runLater(new Runnable() {
-                                      @Override
-                                      public void run() {
-                                          FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/waitingPlayersScene.fxml"));
-                                          try {
-                                              Parent root = loader.load();
-                                              GuiController boardController = loader.getController();
-                                              getClient().removeObserver(MatchPlayersController.this);
-                                              boardController.setStage(getStage());
-                                              boardController.setScene(new Scene(root));
-                                              boardController.setRoot(root);
-                                              getClient().addObserver(boardController);
-                                              boardController.setClient(getClient());
-                                              boardController.getStage().setScene(boardController.getScene());
-                                              boardController.getStage().show();
-                                          } catch(IOException e) {
-                                              //Errore, spero non capiti
+                if (message instanceof SetupMessage) {
+                    Platform.runLater(new Runnable() {
+                                          @Override
+                                          public void run() {
+                                              FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/waitingPlayersScene.fxml"));
+                                              try {
+                                                  Parent root = loader.load();
+                                                  GuiController waitingController = loader.getController();
+                                                  getClient().removeObserver(MatchPlayersController.this);
+                                                  waitingController.setStage(getStage());
+                                                  waitingController.setScene(new Scene(root));
+                                                  waitingController.setRoot(root);
+                                                  getClient().addObserver(waitingController);
+                                                  waitingController.setClient(getClient());
+                                                  waitingController.getStage().setScene(waitingController.getScene());
+                                                  waitingController.getStage().show();
+                                              } catch(IOException e) {
+                                                  //Errore, spero non capiti
+                                              }
                                           }
                                       }
-                                  }
-                );
+                    );
+                }
 
             } else {
                 //Errore server
             }
-        } else {
-            //Errore
+        } else if (message instanceof GameMessage) {
+            Platform.runLater(new Runnable() {
+                                  @Override
+                                  public void run() {
+                                      FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/boardSceneTwoPlayers.fxml"));
+                                      try {
+                                          Parent root = loader.load();
+                                          GuiController boardController = loader.getController();
+                                          getClient().removeObserver(MatchPlayersController.this);
+                                          boardController.setStage(getStage());
+                                          boardController.setScene(new Scene(root));
+                                          boardController.setRoot(root);
+                                          getClient().addObserver(boardController);
+                                          boardController.setClient(getClient());
+                                          boardController.getStage().setScene(boardController.getScene());
+                                          boardController.getStage().show();
+                                      } catch(IOException e) {
+                                          //Errore, spero non capiti
+                                      }
+                                  }
+                              }
+            );
         }
     }
 
