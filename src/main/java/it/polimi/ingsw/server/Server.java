cfdf0cdec0e575727c045ce06a538359380d4737
MichaelVitali <michael.vitali@mail.polimi.it>
2022-06-27 15:01:41 +0200

    endGame changes

--- src/main/java/it/polimi/ingsw/server/Server.java
+++ src/main/java/it/polimi/ingsw/server/Server.java
@@ -51,42 +51,7 @@ public class Server {
             }
         }
     }
-/*
-    private void startGame(Match match){
 
-        View[] playerView = new RemoteView[match.getNumberOfPlayers()];
-        for (int i = 0; i < match.getNumberOfPlayers(); i++) {
-            playerView[i] = new RemoteView(i, match.getPlayerNicknames().get(i), match.getSockets().get(i));
-        }
-        for (int i = 0; i < match.getNumberOfPlayers(); i++) {
-            System.out.println("Player : " + i + " " + match.getPlayerNicknames().get(i) + " " + match.getSockets().get(i).toString());
-        }
-        System.out.println(match.getGameMode());
-        Game model;
-        if (match.getGameMode() == GameMode.NORMAL)
-            model = new Game(match.getNumberOfPlayers(), match.getPlayerNicknames());
-        else
-            model = new ExpertGame(match.getNumberOfPlayers(), match.getPlayerNicknames());
-
-        Controller controller = new Controller(model);
-
-        match.setModel(model);
-
-        for (int i = 0; i < match.getNumberOfPlayers(); i++) {
-            model.addObserver(playerView[i]);
-            playerView[i].addObserver(controller);
-
-            GameMessage displayedBoard = new GameMessage(model, i);
-            match.getSockets().get(i).send(displayedBoard);
-        }
-
-        System.out.println("The match " + match.getMatchId() + " starts");
-        System.out.println("The starting order of match " + match.getMatchId() + " is " + model.getRound().getPlayerOrder().toString());
-
-        runningMatches.add(match);
-        pendingMatches.remove(match);
-    }
-*/
     /**
      *
      * @param clientConnection
